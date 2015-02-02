3f8b14b421bbed03068c4c3fda61105622454172
Brian Ensor <brianensorappstore@gmail.com>
2015-02-01 17:49:54 -0700

    Different functionality for right and left click
    Created RightClick category on NSStatusBarButton using an associated
    variable to store the rightAction property. The category implements
    rightMouseDown: to trigger rightAction on the target.

--- JKey/AppDelegate.m
+++ JKey/AppDelegate.m
@@ -7,6 +7,32 @@
 //
 
 #import "AppDelegate.h"
+#import <objc/runtime.h>
+
+@interface NSStatusBarButton (RightClick)
+
+@property SEL rightAction;
+
+@end
+
+@implementation NSStatusBarButton (RightClick)
+
+@dynamic rightAction;
+
+- (SEL)rightAction {
+    return [objc_getAssociatedObject(self, @selector(rightAction)) pointerValue];
+}
+
+- (void)setRightAction:(SEL)_rightAction {
+    objc_setAssociatedObject(self, @selector(rightAction), [NSValue valueWithPointer:_rightAction], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)rightMouseDown:(NSEvent *)theEvent {
+    [self sendAction:self.rightAction to:self.target];
+    [super rightMouseDown:theEvent];
+}
+
+@end
 
 @interface AppDelegate ()
 
@@ -25,11 +51,16 @@
     _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     _statusItem.button.title = @"J";
     _statusItem.button.target = self;
-    _statusItem.button.action = @selector(inputJKey);
+    _statusItem.button.action = @selector(onLeftClick);
+    _statusItem.button.rightAction = @selector(onRightClick);
 }
 
-- (void)inputJKey {
+- (void)onLeftClick {
     [_appleScript executeAndReturnError:nil];
+}
+
+- (void)onRightClick {
+    NSLog(@"right click");
 }
 
 @end
