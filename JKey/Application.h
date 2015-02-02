71dd899a1fcdc591f4884c5578e43eab266a4647
Shilo <shilo86@gmail.com>
2015-02-01 22:53:12 -0800

    Support for keyboard shortcuts in alert view.

---
+++ JKey/Application.h
@@ -0,0 +1,13 @@
+//
+//  Application.h
+//  JKey
+//
+//  Created by Shilo White on 2/1/15.
+//  Copyright (c) 2015 Shilocity. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Application : NSApplication
+
+@end
---
+++ JKey/Application.m
@@ -0,0 +1,50 @@
+//
+//  Application.m
+//  JKey
+//
+//  Created by Shilo White on 2/1/15.
+//  Copyright (c) 2015 Shilocity. All rights reserved.
+//
+
+#import "Application.h"
+
+@implementation Application
+
+- (void) sendEvent:(NSEvent *)event {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
+    if ([event type] == NSKeyDown) {
+        if (([event modifierFlags] & NSDeviceIndependentModifierFlagsMask) == NSCommandKeyMask) {
+            if ([[event charactersIgnoringModifiers] isEqualToString:@"x"]) {
+                if ([self sendAction:@selector(cut:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"c"]) {
+                if ([self sendAction:@selector(copy:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"v"]) {
+                if ([self sendAction:@selector(paste:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"z"]) {
+                if ([self sendAction:@selector(undo:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"a"]) {
+                if ([self sendAction:@selector(selectAll:) to:nil from:self])
+                    return;
+            }
+        }
+        else if (([event modifierFlags] & NSDeviceIndependentModifierFlagsMask) == (NSCommandKeyMask | NSShiftKeyMask)) {
+            if ([[event charactersIgnoringModifiers] isEqualToString:@"Z"]) {
+                if ([self sendAction:@selector(redo:) to:nil from:self])
+                    return;
+            }
+        }
+    }
+    [super sendEvent:event];
+#pragma clang diagnostic pop
+}
+
+@end
